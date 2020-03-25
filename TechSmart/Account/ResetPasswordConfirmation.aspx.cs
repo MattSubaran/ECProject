20ec603d5fa084195a60b6abd6c2609e683b8495
MATTHEW SUBARAN <subaranmatthew@gmail.com>
2020-03-25 00:24:20 -0500

    Commit All, Uploading Project

---
+++ TechSmart/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace TechSmart.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ TechSmart/Models/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace TechSmart.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("TechSmart")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CartItem> ShoppingCartItems { get; set; }
+    }
+}
